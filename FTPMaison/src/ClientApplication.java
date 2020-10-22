3971ad3fb5fc926c073a51afb49c801b4cf5de79
Paul <pauldrdlg@gmail.com>
2020-10-22 11:35:32 -0400

    update path

--- FTPMaison/src/ClientApplication.java
+++ FTPMaison/src/ClientApplication.java
@@ -1,5 +1,16 @@
+/*
+* Commande pour Paul
+* cd C:\Users\Utilisateur\Documents\GitHub Repository\APP3-S3-A2020\FTPMaison\out\production\FTPMaison
+* java ClientApplication
+*
+* Commande pour Olivier
+* cd 
+* java ClientApplication
+* */
+
 public class ClientApplication {
     public static void main(String[] args) {
+
         System.out.println("Salut Paul!");
     }
 }
